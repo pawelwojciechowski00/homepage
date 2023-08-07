df69a349edab880055c70321cb8f21cc7845aa2a
Paweł <pawlosx350@gmail.com>
2023-08-07 22:27:59 +0200

    DOM

--- main.js
+++ main.js
@@ -3,5 +3,10 @@ const age = 23;
 
 console.log(`Nazywam się ${firstName} i mam ${age} lata`)
 
-const header = document.querySelector('h1')
-console.log(header)
+const header = document.querySelector('.page-header__heading--js')
+
+header.innerHTML = `Cześć jestem ${firstName} i mam ${age} lata a oto moja strona domowa`
+
+console.log(header.style)
+
+header.style.color = 'white';
