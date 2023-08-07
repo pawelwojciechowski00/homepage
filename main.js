4da167bc82789a3065e46b9949984640d5f6567b
Paweł <pawlosx350@gmail.com>
2023-08-07 22:17:00 +0200

    added normalization and divs

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-alert('Witaj!');
 const firstName = "Pawel";
 const age = 23;
 
 console.log(`Nazywam się ${firstName} i mam ${age} lata`)
+
+const header = document.querySelector('h1')
+console.log(header)
